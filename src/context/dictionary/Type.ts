80f0d8c8f40424dbcd2f849a9bc4558246c45e1d
Bruno Silveira <silverdev42@gmail.com>
2025-10-12 01:11:19 -0300

    added better words and difficulty selection

--- src/context/dictionary/Type.ts
+++ src/context/dictionary/Type.ts
@@ -1,26 +1,30 @@
 type Dictionary = {
-	or: string;
-	and: string;
-	greeting: string;
-	userName: string;
-	close: string;
-	welcomeBack: string;
-	welcome: string;
-	loading: string;
-	language: string;
-	game: {
-		self: string;
-		title: string;
-		youWin: string;
-		youLose: string;
-		chooseWord: string;
-		generateRandomWord: string;
-		chooseLetter: string;
-		confirm: string;
-		clear: string;
-		showHideWord: string;
-		wrongLetters: string;
-	}
+    or: string;
+    and: string;
+    greeting: string;
+    userName: string;
+    close: string;
+    welcomeBack: string;
+    welcome: string;
+    loading: string;
+    language: string;
+    game: {
+        self: string;
+        title: string;
+        youWin: string;
+        youLose: string;
+        chooseWord: string;
+        generateRandomWord: string;
+        chooseLetter: string;
+        confirm: string;
+        clear: string;
+        showHideWord: string;
+        wrongLetters: string;
+        difficulty: string;
+        easy: string;
+        medium: string;
+        hard: string;
+    }
 };
 
 export default Dictionary;
