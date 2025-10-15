c41a35fef5fb47867e5de7034e609742ac114209
Bruno Silveira <silverdev42@gmail.com>
2025-10-15 02:12:59 -0300

    Langauge intelligence of getting browser default

--- src/context/model/Intl.ts
+++ src/context/model/Intl.ts
@@ -1,20 +1,20 @@
 import Dictionary from '../dictionary/Type';
 
 export type Languages = {
-	pt_br: string;
-	en: string;
+    pt_br: string;
+    en: string;
 };
 
 export type Currencies = {
-	BRL: string;
-	USD: string;
+    BRL: string;
+    USD: string;
 };
 
 export type Intl = {
-	listLanguages: () => Languages;
-	getLanguage: () => keyof Languages;
-	getCurrency: () => keyof Currencies;
-	getDictionary: () => Dictionary;
-	changeLanguage: (language: keyof Languages) => void;
-	changeCurrency: (currency: keyof Currencies) => void;
+    listLanguages: () => Languages;
+    getLanguage: () => keyof Languages;
+    getCurrency: () => keyof Currencies;
+    getDictionary: () => Dictionary;
+    changeLanguage: (language: keyof Languages) => void;
+    changeCurrency: (currency: keyof Currencies) => void;
 };
